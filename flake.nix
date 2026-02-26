57aab34a2adcb465260a86483de14bd20035d91a
Jordan MacDonald <jordan@wastedintelligence.com>
2026-02-25 22:38:15 -0500

    Bump Nix package channel to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Amp: A complete text editor for your terminal";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }:
